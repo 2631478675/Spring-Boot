34239160cba709071f5de4bf1066207591654cd5
2631478675 <2631478675@qq.com>
2018-05-16 23:05:44 +0800

    resolve bug

--- spring-boot-web-atguigu/src/main/java/springboot/springbootwebatguigu/SpringBootWebAtguiguApplication.java
+++ spring-boot-web-atguigu/src/main/java/springboot/springbootwebatguigu/SpringBootWebAtguiguApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+//@EnableAutoConfiguration
 public class SpringBootWebAtguiguApplication {
 
 	public static void main(String[] args) {
