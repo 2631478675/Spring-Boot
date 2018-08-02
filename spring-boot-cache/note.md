652c147c1841c0bfbf6948e8ccbd765a151a12f4
liuyuanlin <2631478675@qq.com>
2018-08-02 18:32:00 +0800

    intergrate cache but having bug

--- spring-boot-cache/note.md
+++ spring-boot-cache/note.md
@@ -12,4 +12,12 @@
         - allEntries = true：指定清除这个缓存中所有的数据
         - beforeInvocation = false：缓存的清除是否在方法之前执行，默认代表缓存清除操作是在方法执行之后执行;如果出现异常缓存就不会清除
         - beforeInvocation = true：代表清除缓存操作是在方法运行之前执行，无论方法是否出现异常，缓存都清除
-#### 整合redis作为缓存中间件    
+#### [整合redis作为缓存中间件（可以实时查看缓存的内容)](https://www.cnblogs.com/wscy/p/9241747.html)
+- 两个操作redis缓存的template
+    - stringRedisTemplate：操作的k-v都是字符串
+    - redisTemplate ：操作的k-v都是对象
+- redis默认缓存的是序列化后的数据，我们将解决掉这个问题，使得缓存的是json
+    - 引入了redis的starter，cacheManager变为 RedisCacheManager；
+    - 默认创建的 RedisCacheManager 操作redis的时候使用的是 RedisTemplate<Object, Object>,RedisTemplate<Object, Object> 是 默认使用jdk的序列化机制
+    
+![image](http://p3379ff71.bkt.clouddn.com/6.40.48.png)
--- spring-boot-cache/src/test/java/deki/com/springbootcache/SpringBootCacheApplicationTests.java
+++ spring-boot-cache/src/test/java/deki/com/springbootcache/SpringBootCacheApplicationTests.java
@@ -1,16 +1,31 @@
 package deki.com.springbootcache;
 
+import deki.com.springbootcache.entity.Person;
+import deki.com.springbootcache.repository.PersonRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootCacheApplicationTests {
 
+//	@Autowired
+//	RedisTemplate redisTemplate;
+
+	@Autowired
+	PersonRepository personRepository;
+
+	@Autowired
+	RedisTemplate<Object, Person> redisTemplate;
+
 	@Test
 	public void contextLoads() {
+		Person person = personRepository.getOne((long) 1);
+		redisTemplate.opsForValue().set("findByName[合肥]",person);
 	}
 
 }
